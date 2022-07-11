2f028bcc991a978c08bc4fef34f432f540d992c7
Thyago Ribeiro <thyago.ribeiro@webjump.com.br>
2022-07-11 09:31:30 -0400

    Updated nextConfig, package e tsconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+module.exports = {
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: true | {
+      displayName: true,
+      // Enabled by default.
+      ssr: true,
+      // Enabled by default.
+      fileName: true,
+      // Enabled by default.
+      cssProp: true,
+      // Not supported yet.
+      pure: true,
+
+      reactStrictMode: true
+  },
+}
+}
